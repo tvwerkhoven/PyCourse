2bf7a9c7851849a4690c1bf79850e28f76396151
Tim van Werkhoven <t.i.m.vanwerkhoven@xs4all.nl>
2011-10-25 15:40:33 +0200

    Added box/tag images

--- 00-img/getimgs.sh
+++ 00-img/getimgs.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+# Python XKCD
 wget http://imgs.xkcd.com/comics/python.png
+
+# Boxes & tags
+wget http://python.net/~goodger/projects/pycon/2007/idiomatic/a1box.png
+wget http://python.net/~goodger/projects/pycon/2007/idiomatic/a2box.png
+wget http://python.net/~goodger/projects/pycon/2007/idiomatic/b2box.png
+wget http://python.net/~goodger/projects/pycon/2007/idiomatic/a1tag.png
+wget http://python.net/~goodger/projects/pycon/2007/idiomatic/a2tag.png
+wget http://python.net/~goodger/projects/pycon/2007/idiomatic/1.png
+wget http://python.net/~goodger/projects/pycon/2007/idiomatic/ab2tag.png
+
+
